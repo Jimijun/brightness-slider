c9b0aad9e880202bfbfc19adec2c80585ed2f0c6
Jimijun <jimijun0@qq.com>
2025-12-04 19:30:38 +0800

    Prevent multiple instances

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include "MainWindow.h"
 
+#include <QSharedMemory>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    QSharedMemory running_flag("brightness-slider_running");
+    QScopeGuard cleanup(
+        [&running_flag]() {
+            if (running_flag.isAttached()) {
+                running_flag.unlock();
+                running_flag.detach();
+            }
+        }
+    );
+    if (!running_flag.create(1))
+        return 1;
+    running_flag.lock();
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
